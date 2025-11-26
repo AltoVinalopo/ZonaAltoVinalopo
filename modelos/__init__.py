d4eaf2ecbfa51978761cf2078f1e68203cd2c3f7
AltoVinalopo <lorenasanchezlidiar@gmail.com>
2025-11-26 16:39:34 +0100

    Update __init__.py

--- modelos/__init__.py
+++ modelos/__init__.py
@@ -1,10 +1,6 @@
 # modelos/__init__.py
 from flask_sqlalchemy import SQLAlchemy
 
-# 1. Crear instancia global de SQLAlchemy
 db = SQLAlchemy()
 
-# 2. Importar modelos (solo DESPUÉS de crear db)
-from .user import User        # noqa: F401
-from .ayuntamiento import Ayuntamiento  # noqa: F401
-from .zona import Zona        # noqa: F401
+__all__ = ["db"]
