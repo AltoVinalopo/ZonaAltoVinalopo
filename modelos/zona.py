b2d7c34406ec423f8e5d9517803a701a37a6d8ab
AltoVinalopo <lorenasanchezlidiar@gmail.com>
2025-11-26 21:48:12 +0100

    Delete modelos/zona.py

--- modelos/zona.py
+++
@@ -1,7 +0,0 @@
-from modelos import db
-
-class Zona(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
-    descripcion = db.Column(db.String(255))
-    url = db.Column(db.String(255))   # <- NUEVO
