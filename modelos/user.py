2517f316ec14fed948ed1344a4c4353269bbc368
AltoVinalopo <lorenasanchezlidiar@gmail.com>
2025-11-26 16:10:25 +0100

    Update user.py

--- modelos/user.py
+++ modelos/user.py
@@ -1,6 +1,7 @@
+from modelos import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from modelos import db   # ← Import correcto sin circularidad
+
 
 
 class User(UserMixin, db.Model):
