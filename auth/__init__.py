0707fe8984845f109c104fc2aa0ccf774aa8105b
AltoVinalopo <lorenasanchezlidiar@gmail.com>
2025-11-26 12:29:26 +0100

    Update __init__.py

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,4 +1,11 @@
-from flask import Blueprint
+# modelos/__init__.py
+from flask_sqlalchemy import SQLAlchemy
 
-auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
-from . import routes
+# Instancia global de la BBDD
+db = SQLAlchemy()
+
+# Importa los modelos para que SQLAlchemy los conozca
+from .user import User          # noqa: F401
+from .ayuntamiento import Ayuntamiento  # noqa: F401
+from .zona import Zona          # noqa: F401
+# añade aquí más modelos si los tienes
