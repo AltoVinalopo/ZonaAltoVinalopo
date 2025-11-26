fad0d38d1ae2dc7d1ccb093fde6e3a5c1d135fe8
AltoVinalopo <lorenasanchezlidiar@gmail.com>
2025-11-26 21:35:17 +0100

    Update zona.py

--- modelos/zona.py
+++ modelos/zona.py
@@ -1,11 +1,7 @@
 from modelos import db
 
 class Zona(db.Model):
-    __tablename__ = "zonas"
-
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
-
-    ayuntamiento_id = db.Column(db.Integer, db.ForeignKey("ayuntamientos.id"), nullable=False)
-
-    usuarios = db.relationship("User", backref="zona", lazy=True)
+    descripcion = db.Column(db.String(255))
+    url = db.Column(db.String(255))   # <- NUEVO
