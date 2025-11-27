95a29afcd52d43f825c4ff0e02108fc98e6762de
AltoVinalopo <lorenasanchezlidiar@gmail.com>
2025-11-27 12:07:27 +0100

    Update __init__.py

--- modelos/__init__.py
+++ modelos/__init__.py
@@ -1,7 +1,10 @@
+# modelos/__init__.py
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# importa los modelos para que se creen las tablas con create_all()
 from .user import User          # noqa
-from .ayuntamiento import Ayuntamiento  # si existe
+from .ayuntamiento import Ayuntamiento  # noqa
 from .zona import Zona          # noqa
+from .iniciativa import Iniciativa  # noqa
