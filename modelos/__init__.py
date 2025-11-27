bcc2654da7973ef161250a8ccc94acffa238238d
AltoVinalopo <lorenasanchezlidiar@gmail.com>
2025-11-27 10:04:35 +0100

    Update __init__.py

--- modelos/__init__.py
+++ modelos/__init__.py
@@ -1,6 +1,7 @@
-# modelos/__init__.py
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-__all__ = ["db"]
+from .user import User          # noqa
+from .ayuntamiento import Ayuntamiento  # si existe
+from .zona import Zona          # noqa
