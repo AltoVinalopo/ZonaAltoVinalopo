6f10b785705168aec533e21a35f345dd78110241
AltoVinalopo <lorenasanchezlidiar@gmail.com>
2025-11-27 12:44:59 +0100

    Update __init__.py

--- ayuntamientos/__init__.py
+++ ayuntamientos/__init__.py
@@ -1,4 +1,6 @@
+# ayuntamientos/__init__.py
 from flask import Blueprint
 
-aytos_bp = Blueprint('aytos', __name__, url_prefix='/ayuntamientos')
-from . import routes
+aytos_bp = Blueprint("ayuntamientos", __name__, url_prefix="/ayuntamientos")
+
+from . import routes  # noqa
