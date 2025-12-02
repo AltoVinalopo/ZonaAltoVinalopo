6d61f97c507d212658fb102e4072ea62867b1e8e
AltoVinalopo <lorenasanchezlidiar@gmail.com>
2025-12-02 10:45:49 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
-# config.py
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "cambia-esto-por-uno-largo-y-seguro")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "cambia-esto-por-un-secreto-largo-y-seguro")
 
-    # Usamos SQLite por defecto (archivo coordinacion.db en el mismo directorio)
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
         "sqlite:///" + os.path.join(BASE_DIR, "coordinacion.db")
