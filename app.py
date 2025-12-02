1312b5c43b31072a744ed80b5f9ac38e3bfdcc5a
AltoVinalopo <lorenasanchezlidiar@gmail.com>
2025-12-02 19:38:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,12 +20,24 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(int(user_id))
+        try:
+            return User.query.get(int(user_id))
+        except:
+            return None
 
-    # Crear tablas automáticamente en Render
+    # Crear tablas + crear admin si no existe
     with app.app_context():
         db.create_all()
 
+        admin = User.query.filter_by(username="admin").first()
+        if not admin:
+            admin = User(username="admin", role="ADMIN")
+            admin.set_password("admin1234")
+            db.session.add(admin)
+            db.session.commit()
+            print("✔ Usuario admin creado en Render")
+
+    # Blueprints
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(zonas_bp)
     app.register_blueprint(aytos_bp, url_prefix="/ayuntamientos")
@@ -41,4 +53,5 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run()
+
 
